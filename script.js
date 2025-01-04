22c03b9447a24a1f50acb79716b7b82becb1e297
Afza Mehak Ansari <152880255+AfzaMehakAnsari@users.noreply.github.com>
2025-01-05 00:47:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // ================================
+//              Navbar
+// ================================
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+// ================================
 //              Slider
 // ================================
 const track = document.querySelector(".slider-track");
